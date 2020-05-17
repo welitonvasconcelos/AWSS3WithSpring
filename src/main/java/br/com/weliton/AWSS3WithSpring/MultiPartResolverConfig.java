23c1e3a665c7ae7aadc6da5f27dd4167a0623c35
Weliton <weliton.filho@pathfind.com.br>
2020-05-17 13:24:55 -0300

    MultiPartFile Resolver Config

---
+++ src/main/java/br/com/weliton/AWSS3WithSpring/MultiPartResolverConfig.java
@@ -0,0 +1,17 @@
+package br.com.weliton.AWSS3WithSpring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class MultiPartResolverConfig {
+	
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(1000000);
+        return multipartResolver;
+    }
+
+}
