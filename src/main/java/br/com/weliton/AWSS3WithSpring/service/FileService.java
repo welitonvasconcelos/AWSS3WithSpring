de583266472d9f7c5e136ae8e8353d1e215ddce7
Weliton <weliton.filho@pathfind.com.br>
2020-05-17 13:41:42 -0300

    create microservice to get file

--- src/main/java/br/com/weliton/AWSS3WithSpring/service/FileService.java
+++ src/main/java/br/com/weliton/AWSS3WithSpring/service/FileService.java
@@ -2,15 +2,20 @@ package br.com.weliton.AWSS3WithSpring.service;
 
 import java.io.File;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 @Service
 public class FileService {
@@ -21,7 +26,7 @@ public class FileService {
 	@Value("${aws.s3.bucket}")
 	private String bucket;
 	
-	public ResponseEntity<String> uploadFile(MultipartFile file) {
+	public ResponseEntity<?> uploadFile(MultipartFile file) {
 		try {
 			File localFile = new File(file.getOriginalFilename());
 			file.transferTo(localFile);
@@ -32,6 +37,28 @@ public class FileService {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 					.body("Problem in Amazon Acess" + " - " + e.getMessage());
 		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+
+	public ResponseEntity<?> getFile(String fileName) {
+		try {
+			if (s3client.doesObjectExist(bucket, fileName)) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The file does not exist");
+			} else {
+				s3client.getObject(bucket, fileName);
+				S3Object o = s3client.getObject(bucket, fileName);
+			    S3ObjectInputStream s3is = o.getObjectContent();
+			    byte[] bytes = IOUtils.toByteArray(s3is);
+		        HttpHeaders httpHeaders = new HttpHeaders();
+		        httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+		        httpHeaders.setContentLength(bytes.length);
+		        httpHeaders.setContentDispositionFormData("attachment", fileName);
+				return new ResponseEntity<>(bytes, httpHeaders, HttpStatus.OK);
+			}
+		} catch (AmazonServiceException e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Problem in Amazon Acess"+" - "+e.getMessage());
+		} catch(Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
 		}
 	}
