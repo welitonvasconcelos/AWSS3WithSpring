bb6152e4ea823a119b1f49aa409d0749c792f185
Weliton <weliton.filho@pathfind.com.br>
2020-05-17 09:57:52 -0300

    starting the project

---
+++ src/main/java/br/com/weliton/AWSS3WithSpring/Awss3WithSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.weliton.AWSS3WithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awss3WithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Awss3WithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/weliton/AWSS3WithSpring/Awss3WithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weliton.AWSS3WithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Awss3WithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
